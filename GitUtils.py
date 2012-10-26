65002599e185742d6cb0fa055511382903f264e9
Shawn Carrillo <shawn.carrillo+git@gmail.com>
2012-10-25 21:08:41 -0400

    initial commit, testing throwing some git commands to the system via sublime

---
+++ GitUtils.py
@@ -0,0 +1,30 @@
+'''
+# Author: shawn.carrillo+sublime@gmail.com
+# Source: https://github.com/scarrillo/Jeeves-Sublime
+# Version 1.0
+# Date: 2012.10.25
+''' 
+import sublime, sublime_plugin, os, subprocess, thread, time, tail, threading
+
+class GitUtils(sublime_plugin.EventListener):
+	def on_query_context(self, view, key, operator, operand, match_all):
+		if key == "git_status":
+			self.doGit(view)
+			return True
+
+		return None
+
+	def doGit(self, view):
+		projFolders = view.window().folders()
+		for folder in projFolders:
+			#view.window().run_command('exec', {"cmd": ["git", "--git-dir="+str(folder)+"/.git", "--work-tree="+str(folder), "status"]} )
+
+			folderName = folder[folder.rfind('/')+1:]
+			print "Git Status: "+folderName
+			# Print git status
+			log = os.popen("git --git-dir="+str(folder)+"/.git --work-tree="+str(folder)+ " status").readlines()
+			# Print current branch
+			#log = os.popen("git --git-dir="+str(folder)+"/.git --work-tree="+str(folder)+ " branch|grep \\*").readlines()
+			if len(log) != 0:
+				for line in log:
+					print line
---
+++ README.md
@@ -0,0 +1,6 @@
+GitUtils: misc sublime/git utils
+=============
+Just testing at the moment. May turn into something.
+Would be nice if we could add git branch and modified indicators to the Sublime Sidebar.
+
+Easy enough to pass git commands down to the system.
